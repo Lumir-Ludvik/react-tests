cb4239820272568d3b9e1688da5363924fc2275d
Lumir Ludvik <lumirludvik@email.cz>
2022-09-12 22:25:29 +0200

    input with useEffect cleanup

--- src/components/simple-form/simple-form.component.jsx
+++ src/components/simple-form/simple-form.component.jsx
@@ -1,5 +1,23 @@
+import {useEffect, useState} from "react";
+
 export const SimpleFormComponent = () => {
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            console.log("user stopped typing or maybe not idk I just guessed")
+        }, 500);
+
+        return () => {
+            console.log("remove every other timeout");
+            clearTimeout(timeout);
+        }
+    }, [data])
+
     return (
-        <p>Simple form page</p>
+        <>
+            <p>Simple form page</p>
+            <input value={data} onChange={(v) => setData(v.target.value)}></input>
+        </>
     )
 }
